55ad69531213ca12162b96efa31dc2f762c57e63
diegobolzel <30809947+diegobolzel@users.noreply.github.com>
2019-05-20 09:31:39 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="estilos.css">
+    <link href="https://fonts.googleapis.com/css?family=Righteous" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <link rel="stylesheet" href="owl/owl.carousel.min.css">
+    <link rel="stylesheet" href="owl/owl.theme.default.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <div class="contenedor" id="contenedor-menu">
+        <p class="logo">DIEGO BOLAÑOS</p>
+        <nav>
+          <ul>
+            <li>Sobre mí</li>
+            <li>Servicios</li>
+            <li>Mis trabajos</li>
+            <li>¿Por qué trabajar conmigo?</li>
+            <li>Blog</li>
+            <li>Contacto</li>
+          </ul>
+        </nav>
+      </div>
+
+      <div class="contenedor">
+        <div class="contenedor-titulo">
+          <div class="titulo">
+            <h1>Diego Bolaños</h1>
+            <h2>Diseñador & Desarrollador Web</h2>
+            <a href="#">Ver mis trabajos</a>
+
+          </div>
+
+        </div>
+      </div>
+    </header>
+
+    <main>
+      <section id="about">
+        <div class="contenedor">
+          <h3>Sobre mi</h3>
+          <div class="contenido">
+            <div class="about-img">
+              <img src="imagenes/about.png">
+            </div>
+            <div class="about-text">
+              <p>Mi nombre es Diego Bolaños y soy diseñador de páginas web. Tengo la experiencia en desarrollo de sitios web para pequeñas y medianas empresas, hasta tiendas en línea con carrito de compras.</p>
+						  <p>Me aseguro de utilizar siempre las últimas tecnologías de desarrollo web: HTML5, CSS3, jQuery, Wordpress. Si necesitás <span> una página web moderna, funcional, que se adapte a todos los diferentes tipos de pantalla</span>, te aseguro que llegaste al lugar correcto.</p>
+						  <p>Podes ponerte en contacto conmigo a través de Facebook, Linkedin o completando el formulario de contacto que se encuentra al final de mi sitio web.</p>
+						  <a href="#">Ver mis trabajos</a>
+
+            </div>
+
+          </div>
+
+        </div>
+      </section>
+      <section id="servicios">
+        <div class="contenedor">
+          <h3>Servicios</h3>
+          <div class="contenedor-servicios">
+
+            <div class="servicio violeta">
+              <h4>Diseño Web</h4>
+              <p>Páginas web atractivas y funcionales con el objetivo de satisfacer todas tus expectativas. La importancia del diseño es fundamental para tener éxito y atraer a tus visitantes. También lo es la velocidad de carga del sitio web, deben ser rápidos para que el visitante tenga la mejor experiencia</p>
+              <div class="icono">
+                <img src="imagenes/monitor.png">
+              </div>
+              <img src="imagenes/waves.png" class="waves" alt="waves">
+            </div>
+
+            <div class="servicio celeste">
+              <h4>Responsive Design</h4>
+              <p>Hoy en día las páginas web se ven en multitud de dispositivos como tablets, celulares, libros electrónicos, notebooks, PCs, etc. La tecnología móvil es el presente y el futuro. Para eso es importante y me aseguro de que tu sitio web sea compatible con todos los tamaños de pantalla de esta época moderna</p>
+              <div class="icono"><img src="imagenes/celular.png">
+              </div>
+              <img src="imagenes/waves.png" class="waves" alt="waves">
+            </div>
+
+            <div class="servicio violeta">
+              <h4>SEO</h4>
+              <p>Al crear sitios web, tengo muy en cuenta la optimización de motores de búsqueda (SEO). Llevo a cabo prácticas de SEO estándar en cada página del sitio web, lo que permite que tu sitio tenga una clasificación más alta en los resultados de Google y asi generar mas tráfico a la web</p>
+              <div class="icono">
+                <img src="imagenes/seo.png">
+              </div>
+              <img src="imagenes/waves.png" class="waves" alt="waves">
+            </div>
+
+            <div class="servicio celeste">
+              <h4>Autoadministrable</h4>
+              <p>Es importante que puedas administrar tu página web vos mismo, desde cualquier lugar y en cualquier momento. Desarrollo sistemas de gestión de contenido que te permiten ver los datos de tu página web y editar el contenido que quieras, lo que te permite mantenerte actualizado</p>
+              <div class="icono"><img src="imagenes/Wordpress.png">
+              </div>
+              <img src="imagenes/waves.png" class="waves" alt="waves">
+            </div>
+
+            <div class="servicio violeta">
+              <h4>Mantenimiento</h4>
+              <p>Podés contactarme si necesitas actualizar tu página web, agregar nuevas secciones o cualquier cambio que necesites. Esto es perfecto para los sitios web de pequeñas empresas, donde no se necesita un diseñador de páginas web a tiempo completo, sino solo en distintas ocasiones</p>
+              <div class="icono">
+                <img src="imagenes/mante.png">
+              </div>
+              <img src="imagenes/waves.png" class="waves" alt="waves">
+            </div>
+
+            <div class="servicio celeste">
+              <h4>Marketing Digital</h4>
+              <p>Utilizo las herramientas mas importantes como Google Adwords y Google Analytics, para que tu negocio aparezca en los resultados de búsqueda de Google justo cuando alguien te necesita y analizar como los usuarios interactúan con tu página web</p>
+              <div class="icono"><img src="imagenes/marketing.png">
+              </div>
+              <img src="imagenes/waves.png" class="waves" alt="waves">
+            </div>
+
+          </div>
+
+
+
+        </div>
+
+      </section>
+
+      <section id="trabajos">
+        <div class="contenedor">
+          <h3>Mis Trabajos</h3>
+          <div class="contenedor-trabajos owl-carousel owl-theme">
+            <div class="trabajo item">
+              <a href="#"><img src="imagenes/Mockup imac.png"><p>Wodin Digital</p></a>
+            </div>
+
+            <div class="trabajo item">
+              <a href="#"><img src="imagenes/miumis-mockup.png"><p>Miumis</p></a>
+            </div>
+
+
+
+            <div class="trabajo item">
+              <a href="#"><img src="imagenes/cookiee.png"><p>The Cookie Way</p></a>
+            </div>
+
+
+            <div class="trabajo item">
+              <a href="#"><img src="imagenes/sofia-mockup.png"><p>Sofia Zimerman</p></a>
+            </div>
+
+        </div>
+      </div>
+      </section>
+
+      <section id="trabajar-conmigo">
+        <div class="contenedor">
+          <h3>¿Por qué trabajar conmigo</h3>
+          <div class="contenedor-trabajar">
+            <div class="item">
+              <ul>
+                <li><img src="imagenes/velocidad.png"></li>
+                <li>velocidad</li>
+                <li>Me aseguro de que tu web cargue entre 2 y 4 segundos (depende de la cantidad de contenido del sitio). Si una web tarda más de 5 segundos en cargar, el 70% de los usuarios lo cierra y no vuelve a entrar más</li>
+              </ul>
+            </div>
+
+            <div class="item">
+              <ul>
+                <li><img src="imagenes/mantenimiento.png"></li>
+                <li>Asesoramiento</li>
+                <li>Mantengo una comunicación constante con mi cliente, para enseñarle a modificar contenido mediante el panel de Wordpress o para cualquier duda/cambio que quiera hacer en su sitio web</li>
+              </ul>
+            </div>
+
+            <div class="item">
+              <ul>
+                <li><img src="imagenes/mobile-icon.png"></li>
+                <li>Mobile Priority</li>
+                <li>Cada vez mas personas navegan desde sus celulares en cualquier parte del mundo. Me aseguro de que tu web esté optimizada para cualquier tipo de dispositivo móvil</li>
+              </ul>
+
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section id="wordpress">
+        <div class="contenedor">
+          <h3>Wordpress. Lo mejor.</h3>
+          <p>Wordpress es la mejor plataforma del mundo para construir tu sitio web</p>
+          <div class="contenedor-wordpress">
+            <div class="wordpress-img">
+              <img src="imagenes/wordpress2.png" alt="">
+            </div>
+            <div class="wordpress-texto">
+              <ul>
+							 <li>Wordpress es 100% personalizado</li>
+							 <li>Facilidad para agregar funcionalidades extras</li>
+							 <li>Está hecho para el SEO (Posicionamiento en Google)</li>
+							 <li>Infinidad de plantillas premium para elegir</li>
+							 <li>Seguridad. Wordpress se va actualizando constantemente para prevenir hackeos</li>
+							 <li>Fácil de usar</li>
+						  </ul>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section id="hosting">
+        <div class="contenedor">
+          <h3>Hosting. Lo más importante</h3>
+          <p>El hosting es la pieza fundamental de un sitio web. Un buen servicio de hosting garantiza la seguridad y confianza que el usuario necesita en su sitio. Yo trabajo con Siteground, el mejor hosting Wordpress del mundo valorado por las mejores empresas del planeta</p>
+          <div class="contenedor-info">
+            <div class="info">
+              <ul>
+                <li class="grande">¿Necesito tener el mejor hosting del mundo?</li>
+                <li style="margin-bottom:16px">Para cualquier sitio web necesitas el mejor hosting del mundo. A solo US$ 60por año Siteground te ofrece:</li>
+                <li class="iconos"><img class="check" src="imagenes/check.png">Dominio gratuito</li>
+                <li class="iconos"><img class="check" src="imagenes/check.png">Soporte 24/7 de primer nivel</li>
+                <li class="iconos"><img class="check" src="imagenes/check.png">Cuentas ilimitadas de e-mails</li>
+                <li class="iconos"><img class="check" src="imagenes/check.png">Backups diarios</li>
+                <li class="iconos"><img class="check" src="imagenes/check.png">10 GB de espacio</li>
+                <li class="iconos"><img class="check" src="imagenes/check.png">Almacenamiento SSD. Tu web a la máxima velocidad</li>
+              </ul>
+            </div>
+
+            <div class="info">
+              <ul>
+                <li class="grande">¿Que pasa si tengo un hosting de mala calidad?</li>
+                <li style="margin-bottom:16px;">Hay que tratar de no dejarse llevar por lo barato. Contratar un hosting de mala calidad puede provocar:</li>
+                <li class="iconos"><img class="check" src="imagenes/danger.png">Carga lenta del sitio web</li>
+                <li class="iconos"><img class="check" src="imagenes/danger.png">Error 500 del servidor al ingresar al sitio</li>
+                <li class="iconos"><img class="check" src="imagenes/danger.png">Mal posicionamiento en Google</li>
+                <li class="iconos"><img class="check" src="imagenes/danger.png">Soporte insuficiente</li>
+                <li class="iconos"><img class="check" src="imagenes/danger.png">Vulnerabilidad frente a hackers</li>
+                <li class="iconos"><img class="check" src="imagenes/danger.png">No brinda el certificado de seguridad SSL</li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </section>
+
+      <section id="blog">
+        <div class="contenedor">
+          <h3>Blog</h3>
+          <div class="contenido">
+            <div class="blog-single">
+              <div class="blog-img">
+                <a href=""><img src="imagenes/blog1.jpg"></a>
+              </div>
+              <div class="blog-box-content">
+                <h4>No alcanza con tener solamente una página web</h4>
+                <p>El remarketing es una de las técnicas mas utlizadas por las mejores companías! ¿Querés saber de que se trata?</p>
+                <a href="#">Leer más →</a>
+              </div>
+            </div>
+
+            <div class="blog-single">
+              <div class="blog-img">
+                <a href=""><img class="remarketing" src="imagenes/blog2.jpg"></a>
+              </div>
+              <div class="blog-box-content">
+                <h4>¿Qué es el remarketing y como utilizarlo?</h4>
+                <p>¿Querés aprender cuales son las herramientas que usan las mejores marcas para generar tráfico?</p>
+                <a href="#">Leer más →</a>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </section>
+
+      <section id="cta">
+        <div class="contenedor">
+          <h3>¿Qué estperas para tener tu página web?</h3>
+          <a href="#">HABLEMOS</a>
+        </div>
+      </section>
+
+      <section id="contacto">
+        <?php
+          if(@S_GET['i'] == 'ok') {?>
+            <h3>La consulta se envio correctamente. Le contactaré a la mayor brevedad posible</h3>
+            ?<?php
+          } else {
+            // code...
+          } ?>
+          <div class="contenedor">
+            <h3>Contacto</h3>
+            <form class="form" action="mienvio_formulario.php" method="post" enctype="multipart/form-data">
+              <input placeholder="Nombre" type="text" name="nombre">
+              <input placeholder="Email" type="email" name="email">
+              <textarea placeholder="Mensaje" name="mensaje"></textarea>
+              <div id="mensaje4" class="success">
+                <i class="fas fa-check"></i>Mensaje enviado
+              </div><!--"esto viene desde fontawesome.com" tambien existe flaticon.com-->
+              <input type="submit" id="bEnviar" value="Pedir Presupuesto">
+            </form>
+            <?php  ?>
+
+            <div class="contacto-info">
+              <div class="mail">
+                <img src="imagenes/mail-icon.png" alt="icono de mail">
+                diegobolzel@gmail.com
+              </div>
+              <div class="whatsapp">
+                <img src="imagenes/whatsapp-icon.png" alt="">84920222
+
+              </div>
+            </div>
+          </div>
+      </section>
+    </main>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script type="text/javascript" src="owl/owl.carousel.min.js">
+    </script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+      $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        margin:10,
+        nav:true,
+        merge:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+});
+
+    </script>
+  </body>
+</html>
